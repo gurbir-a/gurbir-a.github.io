9ab57cda0b34bdff1c5af7f4742a2bf1d129960b
gurbir-a <16670589+gurbir-a@users.noreply.github.com>
2025-08-12 18:22:55 +0000

    Deploying to gh-pages from @ gurbir-a/gurbir-a.github.io@4a91900550b161e7f2aa226707662bf6ae483a2e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "Gurbir Arora&#39;s CV",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
